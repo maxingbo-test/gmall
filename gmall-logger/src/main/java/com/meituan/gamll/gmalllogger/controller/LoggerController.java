131bb6e8375498f78c4eabf7ce3dff3ac0a210d9
root <admin@example.com>
2020-12-04 12:08:59 +0800

    gittest1

---
+++ gmall-logger/src/main/java/com/meituan/gamll/gmalllogger/controller/LoggerController.java
@@ -0,0 +1,23 @@
+package com.meituan.gamll.gmalllogger.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @RequestMapping(value = "/applog")
+    public String applog(@RequestBody String logString){
+
+        // 日志清洗....
+
+        // 日志输出
+        log.info("info:{}",logString);
+        log.debug("debug:{}",logString);
+        return logString;
+    }
+
+}
