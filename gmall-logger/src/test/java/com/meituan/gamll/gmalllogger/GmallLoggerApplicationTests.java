be1f2a275dd370260fec6db2d2affea983013646
root <admin@example.com>
2020-12-04 13:53:31 +0800

    gittest1111

---
+++ gmall-logger/src/test/java/com/meituan/gamll/gmalllogger/GmallLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meituan.gamll.gmalllogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
